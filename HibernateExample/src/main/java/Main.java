5cc8b07f0e06beb9ec6f69074cff1b6c377f4df2
Mikhail_Marisov <marisov.mikhail@icloud.com>
2022-11-06 17:35:48 +0300

    Initial commit

---
+++ HibernateExample/src/main/java/Main.java
@@ -0,0 +1,30 @@
+
+import models.Auto;
+import models.User;
+import services.UserService;
+
+import java.sql.SQLException;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		UserService userService = new UserService();
+        User user = new User("Masha",26);
+        userService.saveUser(user);
+        Auto ferrari = new Auto("Ferrari", "red");
+        ferrari.setUser(user);
+        user.addAuto(ferrari);
+        Auto ford = new Auto("Ford", "black");
+        ford.setUser(user);
+        user.addAuto(ford);
+        userService.updateUser(user);
+        userService.deleteUser(user);
+        User user3 = userService.findUser(1);
+       
+      
+        
+
+	}
+
+}
